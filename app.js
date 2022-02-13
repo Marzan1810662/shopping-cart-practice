7513d8211094f2f8b12ffb1a1a8db3d1bca7a28b
Marzan1810662 <hassanmarzan17@gmail.com>
2022-02-13 17:45:32 +0600

    functionl commit

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log('I am in a separate file')
+//console.log('I am in a separate file')
+
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+
+    //update product total
+    let productTotal = document.getElementById(product + '-total');
+    console.log(productTotal);
+    productTotal.innerText = productNumber * price;
+
+    //calculate total
+    calculateTotal();
+
+}
+
+function getInputValue(product) {
+    const productNumber = parseInt(document.getElementById(product + '-number').value);
+    return productNumber;
+}
+
+function calculateTotal() {
+
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal /10;
+    total = subTotal + tax;
+    //update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('tota-price').innerText = total;
+
+}
+
+// handle phone increase decreasing events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
+
+//handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+});
